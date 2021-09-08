c00b6624b4fa96ded1e0b8e2df1d6a4208de59b8
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2021-09-08 16:17:37 -0300

    mejora setState

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,8 +1,9 @@
+/// <reference types="react" />
 declare class StateHandler<T> {
     state?: T;
-    setState: (t: T) => void;
-    constructor(sts: (t: T) => void);
+    setState: (value: React.SetStateAction<T>) => void;
+    constructor(sts: (value: React.SetStateAction<T>) => void);
 }
-declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: (t: T) => void) => H, initial_value: T): [T, H];
-declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: (t: T) => void) => H, initial_value?: T): [T | undefined, H];
+declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: (value: React.SetStateAction<T>) => void) => H, initial_value: T): [T, H];
+declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: (value: React.SetStateAction<T>) => void) => H, initial_value?: T): [T | undefined, H];
 export { StateHandler, useStateHandler };
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { useState } from "react";
 class StateHandler<T> {
   
   public state? : T;
-  public setState : ( t : T ) => void
+  public setState : ( value : React.SetStateAction<T> ) => void
 
-  constructor( sts : ( t : T ) => void ) { this.setState = sts }
+  constructor( sts : ( value : React.SetStateAction<T> ) => void ) { this.setState = sts }
 
 }
 
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( t : T ) => void ) => H, initial_value : T) : [T, H] 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( t : T ) => void ) => H, initial_value? : T) : [T | undefined, H] 
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( value : React.SetStateAction<T> ) => void ) => H, initial_value : T) : [T, H] 
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( value : React.SetStateAction<T> ) => void ) => H, initial_value? : T) : [T | undefined, H] 
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( t : T ) => void ) => H, initial_value: T) : [T, H]  {
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : ( value : React.SetStateAction<T> ) => void ) => H, initial_value: T) : [T, H]  {
   const [st, setSt] = useState<T>( initial_value );
   const [handler, ] = useState<H>( () => new handlerClass( setSt ) )  
 
