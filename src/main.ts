ddc4e43c2ed0387fa89c1b4798da1a1025fe83d5
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2021-09-24 11:53:50 -0300

    fix initial state

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,10 @@ function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s :
   if (!handler){
     let new_handler = new handlerClass( setSt );
     setHandler( new_handler );
-    setSt( new_handler.state || st  );
+    if (new_handler.state) 
+      setSt(  new_handler.state );
+    else  
+      new_handler.state = st;
     new_handler.instanceCreated && new_handler.instanceCreated();
     return [ ( new_handler.state || st ), new_handler ]
   }
