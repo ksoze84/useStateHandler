eeafc10ca3bc7d76594b98450f2237fd1fc2f586
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2021-09-27 11:49:07 -0300

    method signatures updated

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -9,6 +9,6 @@ declare abstract class StateHandlerState<T> extends StateHandler<T> {
     abstract state: T;
 }
 declare type HandlerSetter<T> = React.Dispatch<React.SetStateAction<T>>;
-declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>) => H, initial_value: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
+declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>) => H, initial_value: T | (() => T)): [T | undefined, H];
 declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>) => H, initial_value?: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
 export { StateHandler, useStateHandler, HandlerSetter };
