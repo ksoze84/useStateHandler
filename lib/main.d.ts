303044b4c40c4704ed4ceef6973742a49518d1fe
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2022-02-09 11:39:12 -0300

    rollback2

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -2,15 +2,13 @@ import React from "react";
 declare abstract class StateHandler<T> {
     state?: T;
     setState: React.Dispatch<React.SetStateAction<T>>;
-    private instanceCreated?;
-    private init;
-    constructor(sts: HandlerSetter<T>, state?: T);
-    private putState;
+    instanceCreated?: () => void;
+    constructor(sts: HandlerSetter<T>);
 }
 declare abstract class StateHandlerState<T> extends StateHandler<T> {
     abstract state: T;
 }
 declare type HandlerSetter<T> = React.Dispatch<React.SetStateAction<T>>;
-declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value: T | (() => T)): [T, H];
-declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value?: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
+declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>) => H, initial_value: T | (() => T)): [T, H];
+declare function useStateHandler<H extends StateHandler<T>, T>(handlerClass: new (s: HandlerSetter<T>) => H, initial_value?: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
 export { StateHandler, useStateHandler, HandlerSetter };
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,11 @@
 import React from "react"
 
 abstract class StateHandler<T> {
-  
-  state?                    : T;
-  setState                  : React.Dispatch<React.SetStateAction<T>>; 
-  private instanceCreated?  : () => void;
-  private init              : boolean;
+  state?            : T;
+  setState          : React.Dispatch<React.SetStateAction<T>>; 
+  instanceCreated?  : () => void;
 
-  constructor( sts : HandlerSetter<T>, state? : T ) { 
-    this.setState = sts; 
-    this.init = false;
-    
-    if(state) 
-      this.state = state;
-
-    this.instanceCreated && this.instanceCreated(); 
-  }
-
-  private putState( s : T ){
-    if (this.init) 
-      { this.state = s };
-    this.init = true;
-  }
-
+  constructor( sts : HandlerSetter<T> ) { this.setState = sts }
 }
 
 abstract class StateHandlerState<T> extends StateHandler<T> {
@@ -33,14 +16,25 @@ abstract class StateHandlerState<T> extends StateHandler<T> {
 
 type HandlerSetter<T> = React.Dispatch<React.SetStateAction<T>> 
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value : T | (() => T)) : [T , H]
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value? : T | (() => T)) : [ H extends StateHandlerState<T> ? T : T | undefined, H]
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value : T | (() => T)) : [T , H]
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value? : T | (() => T)) : [ H extends StateHandlerState<T> ? T : T | undefined, H]
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value: T | (() => T)) : [T, H]  {
-  const [st, setSt]                 = React.useState<T>( initial_value );    
-  const [handler, ]                 = React.useState<H>( () => new handlerClass( setSt, st ) );
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value: T | (() => T)) : [T, H]  {
+  const [st, setSt]                 = React.useState<T>( initial_value );  
+  const [handler, setHandler ]      = React.useState<H>( );
   
-  handler['putState'](st);
+  if (!handler){
+    const new_handler = new handlerClass( setSt );
+    setHandler( new_handler );
+    if (new_handler.state) 
+      setSt(  new_handler.state );
+    else  
+      new_handler.state = st;
+    new_handler.instanceCreated && new_handler.instanceCreated();
+    return [ ( new_handler.state || st ), new_handler ]
+  }
+  else
+    handler.state = st;
 
   return [ st, handler ];
 }
