ea8d14eb8de7a76d584a8f328cb7681b31efed35
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2022-02-09 11:03:46 -0300

    camia concepto

--- src/main.ts
+++ src/main.ts
@@ -2,43 +2,45 @@ import React from "react"
 
 abstract class StateHandler<T> {
   
-  state?            : T;
-  setState          : React.Dispatch<React.SetStateAction<T>>; 
-  instanceCreated?  : () => void;
+  state?                    : T;
+  setState                  : React.Dispatch<React.SetStateAction<T>>; 
+  private instanceCreated?  : () => void;
+  private init              : boolean;
 
-  constructor( sts : HandlerSetter<T> ) { this.setState = sts }
+  constructor( sts : HandlerSetter<T>, state? : T ) { 
+    this.setState = sts; 
+    this.init = false;
+    
+    if(state) 
+      this.state = state;
+
+    this.instanceCreated && this.instanceCreated(); 
+  }
+
+  private putState( s : T ){
+    if (this.init) 
+      { this.state = s };
+    this.init = true;
+  }
 
 }
 
 abstract class StateHandlerState<T> extends StateHandler<T> {
-  
   abstract state    : T;
-
 }
 
 
 
 type HandlerSetter<T> = React.Dispatch<React.SetStateAction<T>> 
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value : T | (() => T)) : [T , H]
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value? : T | (() => T)) : [ H extends StateHandlerState<T> ? T : T | undefined, H]
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value : T | (() => T)) : [T , H]
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value? : T | (() => T)) : [ H extends StateHandlerState<T> ? T : T | undefined, H]
 
-function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T> ) => H, initial_value: T | (() => T)) : [T, H]  {
-  const [st, setSt]                 = React.useState<T>( initial_value );  
-  const [handler, setHandler ]      = React.useState<H>( );
+function useStateHandler<H extends StateHandler<T>, T>( handlerClass : new ( s : HandlerSetter<T>, state? : T ) => H, initial_value: T | (() => T)) : [T, H]  {
+  const [st, setSt]                 = React.useState<T>( initial_value );    
+  const [handler, ]                 = React.useState<H>( () => new handlerClass( setSt, st ) );
   
-  if (!handler){
-    const new_handler = new handlerClass( setSt );
-    setHandler( new_handler );
-    if (new_handler.state) 
-      setSt(  new_handler.state );
-    else  
-      new_handler.state = st;
-    new_handler.instanceCreated && new_handler.instanceCreated();
-    return [ ( new_handler.state || st ), new_handler ]
-  }
-  else
-    handler.state = st;
+  handler['putState'](st);
 
   return [ st, handler ];
 }
