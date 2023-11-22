b92c500d2f464eeb8a8632d558a03ebbe436da69
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2023-11-22 15:19:02 -0300

    upgrade to readonly

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -9,6 +9,6 @@ declare abstract class StateHandlerState<T> extends StateHandler<T> {
     abstract state: T;
 }
 declare type HandlerSetter<T> = [T, React.Dispatch<React.SetStateAction<T>>];
-declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value: T | (() => T)): [T, H];
-declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value?: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
+declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value: T | (() => T)): readonly [T, H];
+declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value?: T | (() => T)): readonly [H extends StateHandlerState<T> ? T : T | undefined, H];
 export { StateHandler, useStateHandler, HandlerSetter };
